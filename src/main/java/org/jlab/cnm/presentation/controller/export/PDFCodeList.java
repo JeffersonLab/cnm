331ddd15c6d3fee5827d87f11145dd1198874a2e
ryans <ryans@jlab.org>
2025-03-17 10:38:39 -0400

    format

--- src/main/java/org/jlab/cnm/presentation/controller/export/PDFCodeList.java
+++ src/main/java/org/jlab/cnm/presentation/controller/export/PDFCodeList.java
@@ -1,20 +1,11 @@
 package org.jlab.cnm.presentation.controller.export;
 
 import java.io.IOException;
-import java.util.List;
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.jlab.cnm.business.session.ExcelCodeListFacade;
-import org.jlab.cnm.business.session.SectorCodeFacade;
-import org.jlab.cnm.business.session.SystemCodeFacade;
-import org.jlab.cnm.business.session.TypeCodeFacade;
-import org.jlab.cnm.persistence.entity.SectorCode;
-import org.jlab.cnm.persistence.entity.SystemCode;
-import org.jlab.cnm.persistence.entity.TypeCode;
 
 /**
  * @author ryans
